b916cb6b4bad473773885d364e11fe3c776b308a
Tushar Shaw <145246175+TUSHAR-SHAWw@users.noreply.github.com>
2024-01-13 13:35:49 +0530

    Initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+npm install -g aws-cli
 aws lambda update-function-configuration --function-name YourFunctionName --timeout NewTimeoutInSeconds --memory-size NewMemorySizeInMB
