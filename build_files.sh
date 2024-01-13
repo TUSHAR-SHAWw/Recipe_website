a5d1759b81dd8723708bf3e1ca743e23facfa009
Tushar Shaw <145246175+TUSHAR-SHAWw@users.noreply.github.com>
2024-01-13 17:14:43 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+pip install --upgrade awscli
 npm install -g aws-cli
 export PATH=$PATH:$(npm prefix -g)/bin
 aws lambda update-function-configuration --function-name YourFunctionName --timeout NewTimeoutInSecond
