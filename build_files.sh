2830894566a98895acf37c6216935f70ab5def73
Tushar Shaw <145246175+TUSHAR-SHAWw@users.noreply.github.com>
2024-01-13 13:44:47 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
 npm install -g aws-cli
-aws lambda update-function-configuration --function-name YourFunctionName --timeout NewTimeoutInSeconds --memory-size NewMemorySizeInMB
+export PATH=$PATH:$(npm prefix -g)/bin
+aws lambda update-function-configuration --function-name YourFunctionName --timeout NewTimeoutInSecond
